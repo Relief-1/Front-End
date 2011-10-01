ee6b224bff7501d539c8d1296944e8cbd8ef61ed
Charlie McConnell <charlie@charlieistheman.com>
2011-10-01 14:25:29 -0700

    [fix] Clients should cache assets for a week - not six.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var HTTPServer = require('http-server').HTTPServer;
 var httpServer = new HTTPServer({
   root: './public/',
   port: 8080,
-  cache: 3600000
+  cache: 604800
 });
 
 httpServer.start();
