4da566e79b2a3ae7d1bb31f82ed7ca4f033e1b36
Charlie McConnell <charlie@charlieistheman.com>
2011-10-01 13:37:01 -0700

    [dist] Removed files from old locations.

--- js/about_us.js
+++
@@ -1,8 +0,0 @@
-// PINS REMAIN BLUE ONCE SELECTED 
-$(document).ready(function() {
-	var navItems = $('[class^=pin]');
-		navItems.click(function(){
-			 navItems.removeClass('pinactive')
-		 $(this).addClass('pinactive')
-		});
-})
